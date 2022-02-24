e3552df5b92cbe7630689c1be96445f7c08c551d
Perkles <luiis.c.sousa@gmail.com>
2022-02-24 13:22:42 -0300

    Adiciona

---
+++ Calculadora/Calc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora {
+    class Calc {
+
+        public double num, num1, Resultado;
+        public int Opçao;
+
+            
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+
+namespace Calculadora {
+    class Program {
+        static void Main(string[] args) {
+
+            Calc Cal = new Calc();
+
+            Console.WriteLine("Digite o primeiro número");
+            Cal.num = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número");
+            Cal.num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Escolha: ");
+            Console.WriteLine("1 - Soma, 2 - Subtração, 3 - Multiplicação, 4 - Divisão, 0 - Volta");
+            Cal.Opçao = int.Parse(Console.ReadLine());
+            
+            while (Cal.Opçao > 4) {
+                Console.WriteLine("Opção inválida, favor inserir outra!");
+                Cal.Opçao = int.Parse(Console.ReadLine());
+            }
+           
+            switch (Cal.Opçao) {
+                case 0:
+                    Console.WriteLine("Saindo...");
+                    break;
+                case 1:
+                    Cal.Resultado = Cal.num + Cal.num1;
+                    Console.WriteLine("Resultado da soma: " + Cal.Resultado.ToString("F2", CultureInfo.InvariantCulture));
+                    break;
+                case 2:
+                    Cal.Resultado = Cal.num - Cal.num1;
+                    Console.WriteLine("Resultado da divisão: " + Cal.Resultado.ToString("F2", CultureInfo.InvariantCulture));
+                    break;
+                case 3:
+                    Cal.Resultado = Cal.num * Cal.num1;
+                    Console.WriteLine("Resultado da multiplicação: " + Cal.Resultado.ToString("F2", CultureInfo.InvariantCulture));
+                    break;
+                case 4:
+                    while (Cal.num1 == 0) {
+                        Console.WriteLine("Não há divisão por ZERO, favor digitar número novamente: ");
+                        Cal.num1 = double.Parse(Console.ReadLine());
+                    }
+                    Cal.Resultado = Cal.num / Cal.num1;
+                    Console.WriteLine("Resultado da divisão: " + Cal.Resultado.ToString("F2", CultureInfo.InvariantCulture));
+                    break;
+                
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}
